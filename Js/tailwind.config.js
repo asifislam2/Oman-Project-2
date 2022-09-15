171a4e84c445ce2fe7706054a6751006416c72f3
asifislam2 <111923685+asifislam2@users.noreply.github.com>
2022-09-15 14:52:35 +0600

    oman project added

---
+++ Js/tailwind.config.js
@@ -0,0 +1,47 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          gray: '#FF7527',
+          red: '#F10707',
+          navtextcolor: '#203239',
+          banner: '#EEEDDE',
+          boxcolor: '#203239',
+          frombg: '#203239',
+          pluscolor: '#FF7527',
+          ash: '#203239',
+          shado: '#FF7527',
+          basecolor: '#D6D5C7',
+          handel: '#EEEDDE',
+          tccolor: '#FF7527',
+          buttoncolor: '#203239',
+          bordercolor: '#D6D5C7',
+          borderash: '#D6D5C7',
+          futterbutton: '#FF7527',
+          buttertext: '#FF7527',
+          mainfutter: '#203239',
+        },
+
+        maxWidth: {
+            contanier: '1500px',
+        },
+
+        // backgroundImage:{
+
+        //     banner: "url('images/banner.png')",
+        //     image5: "url('images/img5.png')",
+        //     image6: "url('images/img6.png')",
+        //     moreimage: "url('images/moreone.png')",
+        //     moreimage: "url('images/moreone.png')",
+        // }
+
+
+
+      },
+
+      fontFamily:{
+        popins:'Poppins',
+      }
+    }
+  }
